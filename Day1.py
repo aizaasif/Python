9b09b94c0c75d410625c47450ffaf85c04b389ff
aizaasif <aizaasif1718@gmail.com>
2025-07-17 15:52:09 +0500

    Basics of python(variables,print functions,operators,etc...)

---
+++ Day1.py
@@ -0,0 +1,94 @@
+#variables--
+
+name="aiza" #string
+age=19      #int 
+price=25.29 #float
+female=True #boolean data type
+male=False  #boolean data type
+random=None #None data type
+
+#print function
+print("Hello world !")
+print(name)
+print(age)
+print(price)
+a=10
+b=5
+sum=a+b
+print(sum)
+
+#Types of variable--
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(male))
+print(type(female))
+print(type(random))
+
+"""""
+i 
+am 
+a multiline
+comment
+"""
+
+#operators
+print(not(a>b))
+print(a==b)
+print(a**b)
+c=10
+d=15
+sum=c+d
+sum+=10
+print(sum)
+var1=True
+var2=False
+var3=True
+var4=True
+print(var1 and var2 )
+
+#type conversions 
+x=12
+y=12
+var=str(x)
+print(type(var))
+temp=float(y)
+print(type(temp))
+
+#input from the user
+"""" 
+age=input("Enter your age ")
+print("Your age is ",age)
+marks=input("Tell your marks : ")
+print("Your marks are : ",marks)
+"""
+
+#input 2 numbers and calculate their sum 
+num1=int(input("Enter 1st number : "))
+num2=int(input("Enter 2nd number : "))
+sum=num1+num2
+print("The sum of entered numbers is : ",sum)
+
+
+#input side of a square and print its area 
+side=float(input("Enter side of a square : "))
+area=side*side
+print("Area of the square is = ",area)
+
+#input 2 floating numbers and print their average 
+n1=float(input("Enter 1st number : "))
+n2=float(input("Enter 2nd number : "))
+avg=(n1+n2)/2
+print("The average of two numbers is = ",avg)
+
+
+
+
+
+
+
+
+
+
+
+

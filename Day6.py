3855e7c5b86651f48af674484ca661e37d22de42
aizaasif <aizaasif1718@gmailfcom>
2025-07-24 08:44:02 +0500

    Dictionary

---
+++ Day6.py
@@ -0,0 +1,29 @@
+# #Dictionary
+# dict={
+#     "name":"aiza",
+#     "age":19,
+#     "Degree":"AI",
+#     "city":"FSD",
+#     "Weight":45
+# }
+# print(type(dict))
+# print(dict)
+
+# # Accessing the dictionary
+# print(dict["name"])
+
+# #changing
+# dict["name"]="aiza asif" #override
+# dict["fav_color"]="Black" #add key
+
+#Nested Dictionary
+student={
+    "name":"aiza asif",
+    "age":19,
+    "repotr_card":{
+        "eng":92,
+        "math":80,
+        "phy":89
+    }   
+}
+print(student)

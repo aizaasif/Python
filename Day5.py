6b69cac3014c8d7ea748ee60825ee1ee64e13625
aizaasif <aizaasif1718@gmail.com>
2025-07-24 08:12:47 +0500

    Conitional statements,list

---
+++ Day5.py
@@ -0,0 +1,42 @@
+#Tuples 
+# tuple=(1,2,3,4,5,)
+# print(type(tuple))
+# tup=()
+# print(tup)
+# tup=("aiza",)
+# print(type(tup))
+#Tuple methods 
+# tuple=(10,23,78,90,45,67,33,45,67,90)
+# print(tuple.count(90))
+# print(tuple.index(90))
+
+# Practice Program 
+# list=[input("Enter your 1st fav movie : "),
+#     input("Enter your 1st fav movie : "),
+#     input("Enter your 1st fav movie : ")]
+# print("The list of your 3 fav movies is : ",list)
+
+# list=[]
+# list.append(input("Enter 1st movie : "))
+# list.append(input("Enter 2nd movie : "))
+# list.append(input("Enter 3rd movie : "))
+# print(list)
+
+# list=[1,2,3,2,1]
+# list=[1,2,3]
+# list=["a","i","z","a"]
+# copied=list.copy()
+# copied.reverse()
+# if(list==copied):
+#     print("List is a palindrome")
+# else:
+#     print("List is not a palindrome")
+
+# tuple=("C","D","A","A","B","B","A")
+# print(tuple.count("A"))
+
+list=["C","D","A","A","B","B","A"]
+list.sort()
+print(list)
+
+

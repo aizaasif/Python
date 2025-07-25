0d58f941737ecfb478102b587e78f890a9cc67a8
aizaasif <aizaasif1718@gmail.com>
2025-07-25 18:19:26 +0500

    Dictionary

--- Day6.py
+++ Day6.py
@@ -16,14 +16,50 @@
 # dict["name"]="aiza asif" #override
 # dict["fav_color"]="Black" #add key
 
-#Nested Dictionary
-student={
-    "name":"aiza asif",
+# #Nested Dictionary
+# student={
+#     "name":"aiza asif",
+#     "age":19,
+#     "report_card":{
+#         "eng":92,
+#         "math":80,
+#         "phy":89
+#     }   
+# }
+# # print(student)
+# print(student["report_card"]["math"])
+
+#Dictionary Methods
+dict={
+    "name":"aiza",
     "age":19,
-    "repotr_card":{
-        "eng":92,
-        "math":80,
-        "phy":89
-    }   
+    "Degree":"AI",
+    "city":"FSD",
+    "Weight":45
 }
-print(student)
+# print(dict.keys())
+# print(list(dict.keys()))
+# print(dict.values())
+# list=list(dict.values())
+# print(list[0])
+# print(dict.items())
+
+# print(dict.get("name"))
+
+# print(dict.update({"name":"aiza asif","age":20}))
+# print(dict)
+
+#Create a nested dictionary representing students:
+#Access Sara’s grade.
+#Add a new student "Ayesha" with age 20 and grade "A+".
+
+students={
+    "stu1": {"name":"sara", "age":20, "grade":'A+'},
+    "stu2": {"name":"aiza", "age":19, "grade":'B+'}
+}
+
+
+
+
+   
+

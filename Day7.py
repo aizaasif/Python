58be8ecf474867b41db1cb83cf792c429e5a0c7c
aizaasif <aizaasif1718@gmail.com>
2025-07-26 15:30:24 +0500

    variables,datatype,print funtions...

---
+++ Day7.py
@@ -0,0 +1,34 @@
+# Sets 
+
+# Initialization
+# set={1,2,3}
+# print(set)
+# print(type(set))
+
+# set={1,"aiza",True,26.0,(1,2,3)}
+# print(set)
+# print(type(set))
+
+# set={{"name":"aiza"}}
+# print(type(set))
+
+# set={[1,"hello"]}
+# print(type(set))
+
+#Empty Set 
+#add method
+set=set()
+print(set)
+print(type(set))
+set.add(1)
+set.add("aiza")
+set.add(22.90)
+set.add(1)
+print(len(set))
+#remove method
+set.remove("aiza")
+print(set)
+print(len(set))
+
+
+

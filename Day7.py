efbb5f95f1773e6bf8c550c34e98ace87d3892c3
aizaasif <aizaasif1718@gmailfcom>
2025-07-26 15:56:21 +0500

    Sets and set methods

--- Day7.py
+++ Day7.py
@@ -17,18 +17,47 @@
 
 #Empty Set 
 #add method
-set=set()
-print(set)
-print(type(set))
-set.add(1)
-set.add("aiza")
-set.add(22.90)
-set.add(1)
-print(len(set))
+# set=set()
+# print(set)
+# print(type(set))
+# set.add(1)
+# set.add("aiza")
+# set.add(22.90)
+# set.add(1)
+# print(len(set))
+
 #remove method
-set.remove("aiza")
-print(set)
-print(len(set))
+# set.remove("aiza")
+# print(set)
+# print(len(set))
+
+#clear Method
+# set={1,"aiza","asif","python"}
+# set.clear()
+# print(set)
+
+#pop method
+# set.pop()
+# print(set)
+# print(set.pop())
+# print(set.pop())
+# print(set.pop())
+
+#union method 
+# set1={1,2,3,4}
+# set2={2,5,3,4}
+# newset=set1.union(set2)
+# print(set1)
+# print(set2)
+# print(newset)
+
+#intersection method 
+set1={1,2,3,4,5}
+set2={2,4,6}
+set3=set1.intersection(set2)
+print(set3)
+
+
 
 
 

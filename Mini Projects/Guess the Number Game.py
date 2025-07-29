f7befc8443c4cb6dbd0bc9f6c8b9d3f07ff21710
aizaasif <aizaasif1718@gmail.com>
2025-07-29 16:59:33 +0500

    guess the number game

--- Mini Projects/Guess the Number Game.py
+++ Mini Projects/Guess the Number Game.py
@@ -1,19 +1,19 @@
 import random
-secret_no=random.randint(1, 10)
+actual_no=random.randint(1, 10)
 
 i=1
 while(i<=3):
     no=int(input("Enter a number you have only 3 attempts "))
-    if(secret_no==no):
+    if(actual_no==no):
         print("Congratulations! you have guesed the right number ")
         break
-    elif(no>secret_no):
+    elif(no>actual_no):
         print("Your guesed number is high than the actual number ")
     else:
         print("Your guesed number is low than the actual number ")
     i+=1
-    if i > 3 and secret_no != no:
-        print(f"Game Over! The correct number was: {secret_no}")
+    if i > 3 and actual_no != no:
+        print(f"Game Over! The correct number was: {actual_no}")
 
 
 

75a2a4182d40d486d8e275e11b55cd00bfee10f1
aizaasif <aizaasif1718@gmail.com>
2025-08-06 20:10:40 +0500

    OOP classes and objects

--- Daily practice/Day18.py
+++ Daily practice/Day18.py
@@ -1,18 +1,18 @@
-# class student:
-#     def __init__(self,name,marks):
-#         self.name=name
-#         self.marks=marks       
+class student:
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks       
 
 
-#     def cal_avg(self):
-#         sam=0
-#         for i in self.marks:
-#             sam+=i
-#         print(sam/3)
+    def cal_avg(self):
+        sam=0
+        for i in self.marks:
+            sam+=i
+        print(sam/3)
 
 
-# s1=student("Aiza",[100,90,80])
-# s1.cal_avg()
+s1=student("Aiza",[100,90,80])
+s1.cal_avg()
 
 class product:
     def __init__(self,name,price):
@@ -24,9 +24,8 @@ class product:
         sam=0
         for i in self.price:
             sam+=i
-            a+=1
         print(f"Average price is {a} product is",sam/3)
-        
+        a+=1
         
 p1=product("Bread",[100,110,120])
 p1.cal_avg()

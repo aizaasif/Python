b041d5dcb8e79539c8a4554e0800887d626774e7
aizaasif <aizaasif1718@gmail.com>
2025-08-10 11:51:25 +0500

    Saved the files

--- Task2.py
+++ Task2.py
@@ -1,63 +1,63 @@
 #Task 8: Ask the user to enter his subject marks. Find his grade in that subject according to these conditions.
 
-# marks=int(input("Enter your marks : "))
-# if(marks>=90):
-#     print("Your Grade is A+ ")
-# elif(marks>=85 and marks<=89.9):
-#     print("Your Grade is A ")
-# elif(marks>=80 and marks<=84.9):
-#     print("Your Grade is A- ")
-# elif(marks>=75 and marks<=79.9):
-#     print("Your Grade is B+ ")
-# elif(marks>=71 and marks<=74.9):
-#     print("Your Grade is B ")
-# elif(marks>=68 and marks<=70.9):
-#     print("Your Grade is B- ")
-# elif(marks>=64 and marks<=67.9):
-#     print("Your Grade is C+ ")
-# elif(marks>=61 and marks<=63.9):
-#     print("Your Grade is C ")
-# elif(marks>=58 and marks<=60.9):
-#     print("Your Grade is C+ ")
-# elif(marks>=54 and marks<=57.9):
-#     print("Your Grade is D+ ")
-# elif(marks>=50 and marks<=53.9):
-#     print("Your Grade is D ")
-# else:
-#     print("Your Grade is F ")
+marks=int(input("Enter your marks : "))
+if(marks>=90):
+    print("Your Grade is A+ ")
+elif(marks>=85 and marks<=89.9):
+    print("Your Grade is A ")
+elif(marks>=80 and marks<=84.9):
+    print("Your Grade is A- ")
+elif(marks>=75 and marks<=79.9):
+    print("Your Grade is B+ ")
+elif(marks>=71 and marks<=74.9):
+    print("Your Grade is B ")
+elif(marks>=68 and marks<=70.9):
+    print("Your Grade is B- ")
+elif(marks>=64 and marks<=67.9):
+    print("Your Grade is C+ ")
+elif(marks>=61 and marks<=63.9):
+    print("Your Grade is C ")
+elif(marks>=58 and marks<=60.9):
+    print("Your Grade is C+ ")
+elif(marks>=54 and marks<=57.9):
+    print("Your Grade is D+ ")
+elif(marks>=50 and marks<=53.9):
+    print("Your Grade is D ")
+else:
+    print("Your Grade is F ")
 
 # Task 9: User will enter his weight in Kilograms and height in meters (1 M=3.23 Feet). Calculate the BMI of the user and display following: 
 
-# weight=float(input("Enter your weught in Kilograms : "))
-# Height=float(input("Enter your height in Meters : "))
-# H2=(Height*Height)
-# BMI=(weight/H2)
-# print("Your BMI is : ",BMI)    
-# if(BMI<18.5):
-#     print("You are underweight")
-# elif(BMI>=18.5 and BMI<=24.9):
-#     print("You are Normal")
-# elif(BMI>=25 and BMI<=29.9):
-#     print("You are Overweight")
-# elif(BMI>30):
-#     print("You are Obese")
+weight=float(input("Enter your weught in Kilograms : "))
+Height=float(input("Enter your height in Meters : "))
+H2=(Height*Height)
+BMI=(weight/H2)
+print("Your BMI is : ",BMI)    
+if(BMI<18.5):
+    print("You are underweight")
+elif(BMI>=18.5 and BMI<=24.9):
+    print("You are Normal")
+elif(BMI>=25 and BMI<=29.9):
+    print("You are Overweight")
+elif(BMI>30):
+    print("You are Obese")
 
 # Task 10: Write a program to display "NTU" if a number entered by user is a multiple of five , otherwise print "DCS".
 
-# no=int(input("Enter a number : "))
-# if(no%5==0):
-#     print("NTU")
-# else:
-#     print("DCS")
+no=int(input("Enter a number : "))
+if(no%5==0):
+    print("NTU")
+else:
+    print("DCS")
 
 # Task 11: Write a program to check whether the last digit of a number (entered by user) is divisible by 3 or not.
 
-# no=int(input("Enter a number must contains more than 1 digit : "))
-# last=no%10
-# if(last%3==0):
-#     print(f"Number {last} is divisible by 3")
-# else:
-#     print(f"Number {last} is not divisible by 3")
+no=int(input("Enter a number must contains more than 1 digit : "))
+last=no%10
+if(last%3==0):
+    print(f"Number {last} is divisible by 3")
+else:
+    print(f"Number {last} is not divisible by 3")
 
 # Task 12: Implement a currency converter which ask the user to enter value in Pak Rupees and convert in following: 
 
@@ -72,7 +72,7 @@ print("7.UK pound")
 print("8.Us Dollar")
 choice=int(input("Enter your choice, only enter number "))
 if (choice==1):
-    AD=value/102.6
+    AD=(value/102.6)
     print(f"{value} PKR = {AD} Australian Dollar")
 elif (choice==2):
     CY=value/22
@@ -81,7 +81,7 @@ elif (choice==3):
     E=value/168.15
     print(f"{value} PKR = {E} Euro")
 elif (choice==4):
-    QR=value/42.2
+    QR=(value/42.2)
     print(f"{value} PKR = {QR} Qatri Riyal")
 elif (choice==5):
     SR=value/40.95
@@ -95,6 +95,7 @@ elif (choice==7):
 elif (choice==8):
     USD=value/154
     print(f"{value} PKR = {USD} US Dollar")
+
 
 
 
--- Task3.py
+++ Task3.py
@@ -2,79 +2,79 @@
 
 # Task 13: Modify the two-number calculator problem and solve it using functions for add, sub etc. Return the results from these functions and display them in main.
 
-# def add(a, b):
-#     return a + b
+def add(a, b):
+    return a + b
 
-# def sub(a, b):
-#     return a - b
+def sub(a, b):
+    return a - b
 
-# def mul(a, b):
-#     return a * b
+def mul(a, b):
+    return a * b
 
-# def div(a, b):
-#     if b != 0:
-#         return a / b
-#     else:
-#         return "Error: Division by zero"
+def div(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero"
 
-# print("Two Number Calculator")
-# num1 = float(input("Enter first number: "))
-# num2 = float(input("Enter second number: "))
+print("Two Number Calculator")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 
-# print("\nResults:")
-# print(f"Addition: {add(num1, num2)}")
-# print(f"Subtraction: {sub(num1, num2)}")
-# print(f"Multiplication: {mul(num1, num2)}")
-# print(f"Division: {div(num1, num2)}")
+print("\nResults:")
+print(f"Addition: {add(num1, num2)}")
+print(f"Subtraction: {sub(num1, num2)}")
+print(f"Multiplication: {mul(num1, num2)}")
+print(f"Division: {div(num1, num2)}")
 
 # Task 14: Write a function which take the radius of circle as first argument and a char (a or c) as second argument. If second argument is a then return the area of circle. If the second argument is c then return the circumference of circle.
 
-# def function(radius,char):
-#     if char=="A" or char=="a":
-#         Area=3.13*(radius*radius)
-#         print(f"Since, your choice is {char} so Area of circle is : ",Area)
-#     elif char=="C" or char=="c":
-#         circumference=2*3.14*radius
-#         print(f"Since. You entered {char} so Circumference is : ",circumference)
-#     else:
-#         print("Invalid Input")
+def function(radius,char):
+    if char=="A" or char=="a":
+        Area=3.13*(radius*radius)
+        print(f"Since, your choice is {char} so Area of circle is : ",Area)
+    elif char=="C" or char=="c":
+        circumference=2*3.14*radius
+        print(f"Since. You entered {char} so Circumference is : ",circumference)
+    else:
+        print("Invalid Input")
 
-# radius=float(input("Enter the radius of circle : "))
-# char=input("Enter your choice (a or c) ")
+radius=float(input("Enter the radius of circle : "))
+char=input("Enter your choice (a or c) ")
 
-# function(radius,char)
+function(radius,char)
 
 # Task 15: Write a function which takes 5 arguments and print their average. Call this function.
 
-# def avg_calc(a,b,c,d,e):
-#     average=(a+b+c+d+e)/5
-#     print(average)
+def avg_calc(a,b,c,d,e):
+    average=(a+b+c+d+e)/5
+    print(average)
 
-# avg_calc(1,2,3,4,5)
+avg_calc(1,2,3,4,5)
 
 # Task 16: Write two functions add and multiply with five arguments and default values. These functions display result.
 
-# def add(a,b,c,d=4,e=5):
-#     s=a+b+c+d+e
-#     print("Sum of numbers is ",s)
+def add(a,b,c,d=4,e=5):
+    s=a+b+c+d+e
+    print("Sum of numbers is ",s)
 
-# add(1,2,3)
+add(1,2,3)
 
-# def mul(a,b,c,d,e=9):
-#     m=a*b*c*d*e
-#     print("Multiplication of numbers is : ",m)
+def mul(a,b,c,d,e=9):
+    m=a*b*c*d*e
+    print("Multiplication of numbers is : ",m)
 
-# mul(1,2,3,4)
+mul(1,2,3,4)
 
 # Task 17: Write a program to create a function show_employee() using the following conditions.
 # •	It should accept the employee’s name and salary and display both.
 # •	If the salary is missing in the function call then assign default value 9000 to salary
 
-# def show(name,salary=9000):
-#     print("The name of the emloyee is : ",name)
-#     print("The salary of the employee is : ",salary)
+def show(name,salary=9000):
+    print("The name of the emloyee is : ",name)
+    print("The salary of the employee is : ",salary)
 
-# show("Aiza")
+show("Aiza")
 
 # Task 18: Write a program to create a function show_details() using the following conditions.
 # •	It should accept the employee’s name and salary and display both
